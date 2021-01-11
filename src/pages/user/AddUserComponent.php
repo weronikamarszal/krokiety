0bb5771368b1494dfa4eea70bd21cb5a5e497938
Jurek <greenbanzai@gmail.com>
2021-01-11 21:12:35 +0100

    dodanie generowanego ID do loginu

--- src/pages/user/AddUserComponent.php
+++ src/pages/user/AddUserComponent.php
@@ -7,13 +7,11 @@ class AddUserComponent extends BaseFormComponent implements Component
     public function __construct($errors)
     {
         $this->fields = array(
-            //new TextInputField('Login', 'login', $errors['login']),
             new TextInputField('Imię', 'firstName', $errors['firstName']),
             new TextInputField('Nazwisko', 'surname', $errors['surname']),
             new TextInputField('Hasło', 'password', $errors['password']),
             new TextInputField('Numer telefonu', 'phoneNumber', $errors['phoneNumber']),
             new TextInputField('Rola', 'role', $errors['role']),
-            //new TextInputField('Email', 'email', $errors['email']),
         );
 
     }
