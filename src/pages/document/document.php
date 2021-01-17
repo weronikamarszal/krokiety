130ea6c32f5c107ea2a78dac9a8c292eac2b1853
weronikamarszal <wero.marszal@gmail.com>
2021-01-17 19:31:43 +0100

    [5.4][5.5] document - search and filter by date

--- src/pages/document/document.php
+++ src/pages/document/document.php
@@ -4,17 +4,20 @@ global $dbh;
 
 $documentsList = [];
 $pagination = new Pagination("documents");
+$searchValues = new SearchValues(["pagesNumber",
+    ["field" => "documentDate_from", "dbField" => "documentDate", "compare" => ">="],
+    ["field" => "documentDate_to", "dbField" => "documentDate", "compare" => "<="]]);
 
 try {
-    $stmt = $dbh->prepare("SELECT * FROM documents ORDER BY id ASC {$pagination->getQueryPart()}");
-    $stmt->execute();
+    $stmt = $dbh->prepare("SELECT * FROM documents {$searchValues->getCondition()} ORDER BY id ASC {$pagination->getQueryPart()}");
+    $stmt->execute($searchValues->getSearchValues());
     $documentsList = $stmt->fetchAll(PDO::FETCH_CLASS, "Document");
 } catch (Exception $e) {
     throw new Exception($e->getMessage());
 }
 displayMenu(new BaseListPage(
     new DocumentListComponent($documentsList),
-    new DocumentSearchForm(),
+    new DocumentSearchForm($_GET),
     "Dokumenty",
     new PaginatorComponent($pagination->getSize()),
     '/krokiety/index.php/add-document'));
